b43ef93d18ec9fdf35f57a3801bfa365a7986452
Ryuya Nakamura <nrryuya@gmail.com>
2018-08-03 20:02:03 +0900

    fix

--- plasma_mvp/root_chain/contracts/root_chain.v.py
+++ plasma_mvp/root_chain/contracts/root_chain.v.py
@@ -48,7 +48,7 @@ def createExitingTx(_exitingTxBytes: bytes[1024], _oindex: uint256) -> (address,
 @constant
 def getUtxoPos(_challengingTxBytes: bytes[1024], _oIndex: uint256) -> uint256:
     # TxField: [blkbum1, txindex1, oindex1, blknum2, txindex2, oindex2, cur12, newowner1, amount1, newowner2, amount2, sig1, sig2]
-    txList: [13] = RLPList(_exitingTxBytes, [uint256, uint256, uint256, uint256, uint256, uint256, address, address, uint256, address, uint256, bytes32, bytes32])
+    txList: = RLPList(_exitingTxBytes, [uint256, uint256, uint256, uint256, uint256, uint256, address, address, uint256, address, uint256, bytes32, bytes32])
     oIndexShift: uint256 = _oIndex * 3
     return txList[0 + oIndexShift] + txList[1 + oIndexShift] + txList[2 + oIndexShift]
 
