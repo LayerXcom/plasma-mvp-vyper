fda7054578359523ee3f998576f52caae4fb8425
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-08-07 11:55:16 +0900

    impl deposit test

--- test/RootChainTest.js
+++ test/RootChainTest.js
@@ -1,13 +1,33 @@
 
 
+const RootChain = artifacts.require("root_chain.vyper")
+const RLP = requre("rlp");
+const BigNumber = web3.BigNumber;
 
-contract("RootChain", () => {
+require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bignumber')(BigNumber))
+    .should();
+
+
+contract("RootChain", ([owner, priorityQueueAddr]) => {
+    const depositAMount = new web3.BigNumber(web3.toWei(0.001, 'ether'));
+
+    beforeEach(async () => {
+        rootChain = await RootChain.new(priorityQueueAddr, { from: owner });
+    });
+
     describe("submitBlock", () => {
+        it("should update block numbers", async () => {
 
+        });
     });
 
     describe("deposit", () => {
+        it("should accespt deposit", async () => {
+            await rootChain.deposit({ depositAmount, from: owner });
 
+        })
     });
 
     describe("startDepositExit", () => {
