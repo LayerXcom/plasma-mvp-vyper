18b21baacb3b3969a9f0c96ccb9a67424769bf22
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-08-07 12:46:57 +0900

    fix deposit test

--- test/RootChainTest.js
+++ test/RootChainTest.js
@@ -1,6 +1,7 @@
 
 
-const RootChain = artifacts.require("root_chain.vyper")
+const RootChain = artifacts.require("root_chain.vyper");
+const PriorityQueue = artifacts.require("priority_queue.vyper");
 const RLP = require("rlp");
 const BigNumber = web3.BigNumber;
 
@@ -11,7 +12,7 @@ require('chai')
 
 
 contract("RootChain", ([owner, priorityQueueAddr]) => {
-    const depositAmount = new web3.BigNumber(web3.toWei(0.001, 'ether'));
+    const depositAmount = new web3.BigNumber(web3.toWei(0.1, 'ether'));
 
     beforeEach(async () => {
         rootChain = await RootChain.new(priorityQueueAddr, { from: owner });
@@ -25,13 +26,23 @@ contract("RootChain", ([owner, priorityQueueAddr]) => {
 
     describe("deposit", () => {
         it("should accespt deposit", async () => {
+            const blknum = await rootChain.getDepositBlock();
             await rootChain.deposit({ depositAmount, from: owner });
-
+            const depositBlockNum = await rootChain.getDepositBlock();
+            Number(depositBlockNum).should.equal(Number(blknum) + 1);
         })
     });
 
     describe("startDepositExit", () => {
+        it("should be equal ", async () => {
+            await rootChain.deposit({ depositAmount, from: owner });
+            const blknum = await rootChain.getDepositBlock();
+            await rootChain.deposit({ depositAmount, from: owner });
+            const expectedUtxoPos = blknum * 1000000000;
 
+            const expectedExitable_at =
+                await rootChain.startDepositExit()
+        })
     });
 
     describe("startFeeExit", () => {
