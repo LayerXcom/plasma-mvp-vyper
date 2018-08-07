8180bfed7280c308d83ea154adc49e1060ff3a3a
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-08-07 11:29:56 +0900

    impl test skelton

---
+++ test/RootChainTest.js
@@ -0,0 +1,32 @@
+
+
+
+contract("RootChain", () => {
+    describe("submitBlock", () => {
+
+    });
+
+    describe("deposit", () => {
+
+    });
+
+    describe("startDepositExit", () => {
+
+    });
+
+    describe("startFeeExit", () => {
+
+    });
+
+    describe("startExit", () => {
+
+    });
+
+    describe("challengeExit", () => {
+
+    });
+
+    describe("finalizeExits", () => {
+
+    });
+});
