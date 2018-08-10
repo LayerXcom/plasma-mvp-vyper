5f83e2b4cb24184e9828631a627fa78095443891
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-08-10 13:41:13 +0900

    fix

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-const RootChain = artifacts.require("./plasma_mvp/root_chain/contracts/root_chain.vyper");
-const PriorityQueue = artifacts.require("./plasma_mvp/root_chain/contracts/priority_queue.vyper");
+const RootChain = artifacts.require("./plasma_mvp/root_chain.vyper");
+const PriorityQueue = artifacts.require("./plasma_mvp/priority_queue.vyper");
 
 module.exports = (deployer) => {
     deployer.deploy(PriorityQueue)
