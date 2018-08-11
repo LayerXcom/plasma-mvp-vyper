d51a5932f3d992d0f1991f461df3ee5d1aaaf864
中村龍矢 <nrryuya@gmail.com>
2018-08-12 08:27:40 +0900

    fix commnet

--- contracts/plasma_mvp/root_chain.v.py
+++ contracts/plasma_mvp/root_chain.v.py
@@ -140,7 +140,7 @@ def getExit(_utxoPos: uint256) -> (address, address, uint256):
 @constant
 def getNextExit(_token: address) -> (uint256, uint256):
     priority: uint256 = PriorityQueue(self.exitsQueues[_token]).getMin()
-    # Cut the first 128 digits represents exitable_at.
+    # Cut the first 128 digits which represents exitable_at.
     utxoPos: uint256 = convert(convert(priority, "int128"), "uint256")
     exitable_at: uint256 = shift(priority, -128)
     return utxoPos, exitable_at
