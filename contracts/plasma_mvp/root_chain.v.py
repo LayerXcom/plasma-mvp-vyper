2889b29d405e0048c5ea9e29982a6f55337a34d1
中村龍矢 <nrryuya@gmail.com>
2018-08-13 00:20:20 +0900

    check the size of signature

--- contracts/plasma_mvp/root_chain.v.py
+++ contracts/plasma_mvp/root_chain.v.py
@@ -60,6 +60,8 @@ def getUtxoPos(_challengingTxBytes: bytes[1024], _oIndex: uint256) -> uint256:
 @private
 @constant
 def ecrecoverSig(_txHash: bytes32, _sig: bytes[65]) -> address:
+    if len(_sig) != 65:
+        return ZERO_ADDRESS
     # ref. https://gist.github.com/axic/5b33912c6f61ae6fd96d6c4a47afde6d
     # The signature format is a compact form of:
     # {bytes32 r}{bytes32 s}{uint8 v}
