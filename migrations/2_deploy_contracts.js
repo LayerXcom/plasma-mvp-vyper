8c7106333ff5c307ca4d726c936fcc1fe8ceab33
中村龍矢 <nrryuya@gmail.com>
2018-08-12 12:17:10 +0900

    rename artifacts which truper made to overwrite them in truffle migation

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,9 @@
-const RootChain = artifacts.require("./plasma_mvp/root_chain.vyper");
-const PriorityQueue = artifacts.require("./plasma_mvp/priority_queue.vyper");
+var fs = require("fs");
+// rename artifacts which truper made to overwrite them in truffle migation
+fs.rename("../build/contracts/plasma_mvp/root_chain.vyper.json", "../build/contracts/plasma_mvp/root_chain.json", function (err) {});
+fs.rename("../build/contracts/plasma_mvp/priority_queue.vyper.json", "../build/contracts/plasma_mvp/priority_queue.json", function (err) {});
+const RootChain = artifacts.require("root_chain");
+const PriorityQueue = artifacts.require("priority_queue");
 
 module.exports = (deployer) => {
     deployer.deploy(PriorityQueue)
