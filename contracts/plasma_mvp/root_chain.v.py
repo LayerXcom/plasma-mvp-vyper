a2dc7be37223523e7270fef4960916cb503ee13d
osuke <arget-fee.free.dgm@hotmail.co.jp>
2018-08-12 15:18:01 +0900

    fix

--- contracts/plasma_mvp/root_chain.v.py
+++ contracts/plasma_mvp/root_chain.v.py
@@ -143,7 +143,7 @@ def getExit(_utxoPos: uint256) -> (address, address, uint256):
 @public
 @constant
 def getNextExit(_token: address) -> (uint256, uint256):
-    priority: uint256 = PriorityQueue(self.exitsQueues[_token]).getMin()    
+    priority: uint256 = PriorityQueue(self.exitsQueues[_token]).getMin()
     # Cut the first 128 digits represents exitable_at.
     utxoPos: uint256 = shift(shift(priority, 128), -128)
     exitable_at: uint256 = shift(priority, -128)
